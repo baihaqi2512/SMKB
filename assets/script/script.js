e2df492f182069932629ab018f19d45bc84a6736
baihaqi2512 <baihaqi2596@gmail.com>
2023-02-20 22:53:35 +0800

    updated

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,6 +9,66 @@ window.onload = function() {
   }, 5500);
 };
 
+// STEP FORM
+
+$(document).ready(function() {
+  var navListItems = $('div.setup-panel div a');
+  var allWells = $('.setup-content');
+  var allNextBtn = $('.nextBtn');
+  var allPrevBtn = $('.prevBtn');
+
+  allWells.hide();
+
+  navListItems.click(function(e) {
+    e.preventDefault();
+    var $target = $($(this).attr('href'));
+    var $item = $(this);
+
+    if (!$item.hasClass('disabled')) {
+      navListItems.removeClass('btn-primary').addClass('btn-default');
+      $item.addClass('btn-primary');
+      allWells.hide();
+      $target.show();
+      $target.find('input:eq(0)').focus();
+    }
+  });
+
+  allPrevBtn.click(function() {
+    var curStep = $(this).closest(".setup-content");
+    var curStepBtn = curStep.attr("id");
+    var prevStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().prev().children("a");
+
+    prevStepWizard.removeAttr('disabled').trigger('click');
+  });
+
+  allNextBtn.click(function() {
+    var curStep = $(this).closest(".setup-content");
+    var curStepBtn = curStep.attr("id");
+    var nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a");
+    var curInputs = curStep.find("input[type='text'],input[type='url']");
+    var isValid = true;
+
+    $(".form-group").removeClass("has-error");
+    for (var i = 0; i < curInputs.length; i++) {
+      if (!curInputs[i].validity.valid) {
+        isValid = false;
+        $(curInputs[i]).closest(".form-group").addClass("has-error");
+      }
+    }
+
+    if (isValid)
+      nextStepWizard.removeAttr('disabled').trigger('click');
+  });
+
+  $('div.setup-panel div a.btn-primary').trigger('click');
+
+  $('.stepwizard-step').click(function() {
+    var $p = $(this).find('p');
+    $('.stepwizard-step p').css('font-weight', 'normal');
+    $p.css('font-weight', 'bold');
+  });
+});
+
 // <-- SEARCHABLE DROPDOWN -->
 $(document).ready(function () {
   $('.searchable-dropdown').selectize({
